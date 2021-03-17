0613cfb0e5e62ac7a4a924b253206857ff43b9ab
brian m. carlson <bk2204@github.com>
2021-03-17 18:16:07 +0000

    ssh: implement protocol handling for pure SSH-based protocol
    With the pure SSH-based protocol, we have a couple different types of
    messages that can be sent and received.  Let's implement generic helpers
    for these types so we can use them later on when we implement the actual
    protocol.

---
+++ ssh/protocol.go
@@ -0,0 +1,275 @@
+package ssh
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/git-lfs/git-lfs/errors"
+	"github.com/git-lfs/git-lfs/subprocess"
+)
+
+type PktlineConnection struct {
+	mu  sync.Mutex
+	cmd *subprocess.Cmd
+	pl  Pktline
+}
+
+func (conn *PktlineConnection) Lock() {
+	conn.mu.Lock()
+}
+
+func (conn *PktlineConnection) Unlock() {
+	conn.mu.Unlock()
+}
+
+func (conn *PktlineConnection) Start() error {
+	conn.Lock()
+	defer conn.Unlock()
+	return conn.negotiateVersion()
+}
+
+func (conn *PktlineConnection) End() error {
+	conn.Lock()
+	defer conn.Unlock()
+	err := conn.SendMessage("quit", nil, true)
+	if err != nil {
+		return err
+	}
+	_, _, err = conn.ReadStatus(true)
+	conn.cmd.Wait()
+	return err
+}
+
+func (conn *PktlineConnection) readCapabilities() ([]string, error) {
+	return conn.pl.ReadPacketList()
+}
+
+func (conn *PktlineConnection) negotiateVersion() error {
+	pkts, err := conn.readCapabilities()
+	if err != nil {
+		return errors.NewProtocolError("Unable to negotiate version with remote side (unable to read capabilities)", err)
+	}
+	ok := false
+	for _, line := range pkts {
+		if line == "version=1" {
+			ok = true
+		}
+	}
+	if !ok {
+		return errors.NewProtocolError("Unable to negotiate version with remote side (missing version=1)", nil)
+	}
+	err = conn.SendMessage("version 1", nil, false)
+	if err != nil {
+		return errors.NewProtocolError("Unable to negotiate version with remote side (unable to send version)", err)
+	}
+	status, _, err := conn.ReadStatus(true)
+	if err != nil {
+		return errors.NewProtocolError("Unable to negotiate version with remote side (unable to read status)", err)
+	}
+	if status != 200 {
+		return errors.NewProtocolError(fmt.Sprintf("Unable to negotiate version with remote side (unexpected status %d)", status), nil)
+	}
+	return nil
+}
+
+func (conn *PktlineConnection) SendMessage(command string, args []string, delim bool) error {
+	err := conn.pl.WritePacketText(command)
+	if err != nil {
+		return err
+	}
+	if delim {
+		err = conn.pl.WriteDelim()
+		if err != nil {
+			return err
+		}
+	}
+	for _, arg := range args {
+		err = conn.pl.WritePacketText(arg)
+		if err != nil {
+			return err
+		}
+	}
+	return conn.pl.WriteFlush()
+}
+
+func (conn *PktlineConnection) SendMessageWithArguments(command string, args []string, lines []string) error {
+	err := conn.pl.WritePacketText(command)
+	if err != nil {
+		return err
+	}
+	for _, arg := range args {
+		err = conn.pl.WritePacketText(arg)
+		if err != nil {
+			return err
+		}
+	}
+	err = conn.pl.WriteDelim()
+	if err != nil {
+		return err
+	}
+	for _, line := range lines {
+		err = conn.pl.WritePacketText(line)
+		if err != nil {
+			return err
+		}
+	}
+	return conn.pl.WriteFlush()
+}
+
+func (conn *PktlineConnection) SendMessageWithData(command string, args []string, data io.Reader) error {
+	err := conn.pl.WritePacketText(command)
+	if err != nil {
+		return err
+	}
+	for _, arg := range args {
+		err = conn.pl.WritePacketText(arg)
+		if err != nil {
+			return err
+		}
+	}
+	err = conn.pl.WriteDelim()
+	if err != nil {
+		return err
+	}
+	buf := make([]byte, 32768)
+	for {
+		n, err := data.Read(buf)
+		if n > 0 {
+			conn.pl.WritePacket(buf[0:n])
+		}
+		if err != nil {
+			break
+		}
+	}
+	return conn.pl.WriteFlush()
+}
+
+func (conn *PktlineConnection) ReadStatus(delim bool) (int, []string, error) {
+	args := make([]string, 0, 100)
+	status := 0
+	seenDelim := false
+	seenStatus := false
+	for {
+		s, pktLen, err := conn.pl.ReadPacketTextWithLength()
+		if err != nil {
+			return 0, nil, nil
+		}
+		switch {
+		case pktLen == 0:
+			if status == 0 {
+				return 0, nil, errors.NewProtocolError("no status seen", nil)
+			} else if !seenDelim {
+				return 0, nil, errors.NewProtocolError("no delimiter seen", nil)
+			}
+			return status, args, nil
+		case pktLen == 1:
+			seenDelim = true
+		case seenDelim:
+			args = append(args, s)
+		default:
+			if strings.HasPrefix(s, "status ") {
+				status, err = strconv.Atoi(s[7:])
+				if err != nil {
+					return 0, nil, errors.NewProtocolError(fmt.Sprintf("expected status line, got %q", s), err)
+				}
+				if !delim {
+					seenDelim = true
+				}
+			}
+			if !seenStatus {
+				seenStatus = true
+				ok := false
+				if strings.HasPrefix(s, "status ") {
+					status, err = strconv.Atoi(s[7:])
+					ok = err != nil
+				}
+				if ok {
+					return 0, nil, errors.NewProtocolError(fmt.Sprintf("expected status line, got %q", s), err)
+				}
+			}
+			// Otherwise, this is an optional argument which we are
+			// ignoring.
+		}
+	}
+}
+
+// ReadStatusWithData reads a status, arguments, and any binary data.  Note that
+// the reader must be fully exhausted before invoking any other read methods.
+func (conn *PktlineConnection) ReadStatusWithData() (int, []string, io.Reader, error) {
+	args := make([]string, 0, 100)
+	status := 0
+	seenStatus := false
+	for {
+		s, pktLen, err := conn.pl.ReadPacketTextWithLength()
+		if err != nil {
+			return 0, nil, nil, nil
+		}
+		if pktLen == 0 {
+			if status == 0 {
+				return 0, nil, nil, errors.NewProtocolError("no status seen", nil)
+			}
+			return status, args, nil, nil
+		} else if pktLen == 1 {
+			break
+		} else {
+			if !seenStatus {
+				seenStatus = true
+				ok := false
+				if strings.HasPrefix(s, "status ") {
+					status, err = strconv.Atoi(s[7:])
+					ok = err != nil
+				}
+				if ok {
+					return 0, nil, nil, errors.NewProtocolError(fmt.Sprintf("expected status line, got %q", s), err)
+				}
+			} else {
+				args = append(args, s)
+			}
+		}
+	}
+
+	return status, args, pktlineReader(conn.pl), nil
+}
+
+// ReadStatusWithArguments reads a status, arguments, and a set of text lines.
+func (conn *PktlineConnection) ReadStatusWithArguments() (int, []string, []string, error) {
+	args := make([]string, 0, 100)
+	lines := make([]string, 0, 100)
+	status := 0
+	seenDelim := false
+	seenStatus := false
+	for {
+		s, pktLen, err := conn.pl.ReadPacketTextWithLength()
+		if err != nil {
+			return 0, nil, nil, err
+		}
+		switch {
+		case pktLen == 0:
+			if status == 0 {
+				return 0, nil, nil, errors.NewProtocolError("no status seen", nil)
+			}
+			return status, args, lines, nil
+		case pktLen == 1:
+			seenDelim = true
+		case seenDelim:
+			lines = append(lines, s)
+		default:
+			if !seenStatus {
+				seenStatus = true
+				ok := false
+				if strings.HasPrefix(s, "status ") {
+					status, err = strconv.Atoi(s[7:])
+					ok = err != nil
+				}
+				if ok {
+					return 0, nil, nil, errors.NewProtocolError(fmt.Sprintf("expected status line, got %q", s), err)
+				}
+			} else {
+				args = append(args, s)
+			}
+		}
+	}
+}
