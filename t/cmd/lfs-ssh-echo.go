34c6ba6572de2937262033721123ab22f96b51d3
brian m. carlson <bk2204@github.com>
2021-05-14 17:47:40 +0000

    t/cmd: expand functionality of lfs-ssh-echo
    Currently, the only thing that lfs-ssh-echo does is provide the output
    for git-lfs-authenticate.  However, in the future, we'll want to support
    git-lfs-transfer, and for our testing functionality, we'll want to
    pretend to have an SSH transport for cloning and pushing.
    
    In order to do so, let's refactor out some code.  Let's plan to spawn a
    shell with the argument if we get a Git command or a git-lfs-transfer
    command, and let's make things work properly on both Unix (where we'll
    want sh) and Windows (where sh is not always available but we can rely
    on bash).
    
    Let's also gracefully handle the fact that we may not always have the
    same number of arguments.  Git will think we're a simple transport
    helper and so we won't receive a port in the tests that use mock SSH
    URLs, so let's accommodate the lack of port argument and the lack of
    -- arguments passed by Git.
    
    Since the error handling has changed somewhat, update the SSH test to
    make it look for the new error message and extract the command line from
    the trace output, since we no longer print it as part of the error
    message in this case.

--- t/cmd/lfs-ssh-echo.go
+++ t/cmd/lfs-ssh-echo.go
@@ -6,7 +6,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -17,33 +20,76 @@ type sshResponse struct {
 	ExpiresIn int               `json:"expires_in,omitempty"`
 }
 
+func shell() string {
+	if runtime.GOOS == "windows" {
+		return "bash"
+	}
+	return "sh"
+}
+
+func spawnCommand(command string) error {
+	cmd := exec.Command(shell(), "-c", command)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok {
+		var ws syscall.WaitStatus
+		ws, ok = e.ProcessState.Sys().(syscall.WaitStatus)
+		if ok {
+			os.Exit(ws.ExitStatus())
+		}
+	}
+	return err
+}
+
 func main() {
 	// expect args:
 	//   lfs-ssh-echo -p PORT -- git@127.0.0.1 git-lfs-authenticate REPO OPERATION
-	if len(os.Args) != 6 {
+	if len(os.Args) < 3 {
 		fmt.Fprintf(os.Stderr, "got %d args: %v", len(os.Args), os.Args)
 		os.Exit(1)
 	}
 
-	if os.Args[1] != "-p" {
-		fmt.Fprintf(os.Stderr, "$1 expected \"-p\", got %q", os.Args[1])
+	offset := 4
+	if os.Args[1] == "--" {
+		offset = 2
+	} else if os.Args[1] == "git@127.0.0.1" {
+		offset = 1
+	} else {
+		if os.Args[1] != "-p" {
+			fmt.Fprintf(os.Stderr, "$1 expected \"-p\", got %q", os.Args[1])
+			os.Exit(1)
+		}
+
+		if os.Args[3] != "--" {
+			fmt.Fprintf(os.Stderr, "$3 expected \"--\", got %q", os.Args[3])
+			os.Exit(1)
+		}
+	}
+
+	if len(os.Args) < offset+2 {
+		fmt.Fprintf(os.Stderr, "got %d args: %v", len(os.Args), os.Args)
 		os.Exit(1)
 	}
 
-	if os.Args[3] != "--" {
-		fmt.Fprintf(os.Stderr, "$3 expected \"--\", got %q", os.Args[3])
-		os.Exit(1)
-	}
-
-	if os.Args[4] != "git@127.0.0.1" {
-		fmt.Fprintf(os.Stderr, "$4 expected \"git@127.0.0.1\", got %q", os.Args[4])
+	if os.Args[offset] != "git@127.0.0.1" {
+		fmt.Fprintf(os.Stderr, "$4 expected \"git@127.0.0.1\", got %q", os.Args[offset])
 		os.Exit(1)
 	}
 
 	// just "git-lfs-authenticate REPO OPERATION"
-	authLine := strings.Split(os.Args[5], " ")
-	if len(authLine) < 13 {
+	authLine := strings.Split(os.Args[offset+1], " ")
+	if len(authLine) < 2 {
 		fmt.Fprintf(os.Stderr, "bad git-lfs-authenticate line: %s\nargs: %v", authLine, os.Args)
+	}
+
+	if authLine[0] == "git-lfs-transfer" || authLine[0] == "git-upload-pack" || authLine[0] == "git-receive-pack" {
+		err := spawnCommand(os.Args[offset+1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error running command %q: %v", authLine[0], err)
+		}
+		return
 	}
 
 	repo := authLine[1]
