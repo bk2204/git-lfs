93e519c3eab33c3553e138033a32782f20720516
brian m. carlson <bk2204@github.com>
2021-05-14 18:33:44 +0000

    t: add a test helper to compute the SSH remote for a repo
    Let's add a test helper to compute the SSH remote for a repository
    suitable for use with both Git and Git LFS if we want to use
    git-lfs-transfer operations.

--- t/testhelpers.sh
+++ t/testhelpers.sh
@@ -810,3 +810,10 @@ setup_pure_ssh() {
       exit 0
   fi
 }
+
+ssh_remote() {
+  local reponame="$1"
+  local destination=$(urlify "$(canonical_path "$REMOTEDIR/$reponame.git")")
+  [ -z "${destination##/*}" ] || destination="/$destination"
+  echo "ssh://git@127.0.0.1$destination"
+}
